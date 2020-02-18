70eb5c9a986d9e270c89613fc8871ffa6b773d31
Alexis Panyathong <alexispanyathong@Alexiss-Air.lan>
2020-02-18 15:48:32 -0600

    Progress:
    I just added my notes to what needs to be done and I also copied and pasted the graph.py and util.py into the ancestor folder.

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,20 @@
+from graph import Graph
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    # Add vertex for parent and child
+    
+    # Need to have to for loops for vertex and edge otherwise it throws an error.
+    # Add edge for parent/child pair written as (parent, child).
+    
+    # New path list
+
+    # For each parents, find path to starting node.
+        # Set path to g.dfs that passes in the parent and starting_node.
+        
+        
+        # If the path id not None & the length of the path is greater than the length of the new path.
+            # Then the new path will replace the old path with the copy().
+            
+    # If the length of the new path is less than or equal to 1. Return -1.
+   
+    # Otherwise return the new path at the index of zero.
